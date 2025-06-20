fe4c2b786bc2f0ba5015dcfc546770c75e093e59
Lian <leeannetala@iskolarngbayan.pup.edu.ph>
2025-06-20 10:18:34 +0800

    Add MultiplyAndShow function

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ class Program
         GreetUser("Lian");
         int result = Add(17, 11);
         Console.WriteLine("Sum: " + result);
+        MultiplyAndShow(7, 7);
     }
 
     static void GreetUser(string name)
@@ -18,5 +19,11 @@ class Program
     static int Add(int a, int b)
     {
         return a + b;
+    }
+
+    static void MultiplyAndShow(int x, int y)
+    {
+        int product = x * y;
+        Console.WriteLine("Product: " + product);
     }
 }

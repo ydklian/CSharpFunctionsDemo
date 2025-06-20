89dc714ce1598305275529277a6dc8b35d12d65c
Lian <leeannetala@iskolarngbayan.pup.edu.ph>
2025-06-20 10:13:03 +0800

    Initial commit: simple C# function demo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== C# Functions Demo ===");
+        GreetUser("Lian");
+        int result = Add(17, 11);
+        Console.WriteLine("Sum: " + result);
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine("Hello, " + name + "!");
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
